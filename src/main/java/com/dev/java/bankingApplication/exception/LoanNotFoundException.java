a5e2addcb4b614b9f68dcd76b4e4f6ba35bb9f65
DebjeetRoy <debjeetroy1992@gmail.com>
2024-07-20 10:01:05 +0530

    Revert "Banking application Project"
    This reverts commit 64f715da2f4417c447341d7a98f3f5baaa23ec72.
    
    t Please enter the commit message for your changes. Lines starting

--- src/main/java/com/dev/java/bankingApplication/exception/LoanNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dev.java.bankingApplication.exception;
-
-public class LoanNotFoundException extends RuntimeException{
-    public LoanNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/dev/java/bankingApplication/repository/LoanTransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dev.java.bankingApplication.repository;
-
-import com.dev.java.bankingApplication.entity.LoanTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoanTransactionRepository extends JpaRepository<LoanTransaction, Long> {
-}
